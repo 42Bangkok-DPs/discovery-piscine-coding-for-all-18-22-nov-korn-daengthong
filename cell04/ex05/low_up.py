010a6470547eb3a87981458d034fbd0885d753f8
korn-daengthong <67030268@kmitl.ac.th>
2024-11-20 18:16:36 +0000

    up_low

---
+++ cell04/ex05/low_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main():
+
+    user_input = input("Please enter a string: ")
+
+    swapped_case = user_input.swapcase()
+    
+    print(swapped_case)
+
+if __name__ == "__main__":
+    main()
+
